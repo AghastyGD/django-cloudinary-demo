2f74c685fa78ae8b0be18f95c4186dd40400b459
AghastyGD <augustodomingosvasco@gmail.com>
2024-07-16 09:57:39 +0200

    add post model

--- demo/admin.py
+++ demo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import PhotoUpload
+from .models import PhotoUpload, Post
 
 admin.site.register(PhotoUpload)
+admin.site.register(Post)
--- demo/models.py
+++ demo/models.py
@@ -3,3 +3,8 @@ from cloudinary.models import CloudinaryField
 
 class PhotoUpload(models.Model):
     image = CloudinaryField('image')
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = CloudinaryField("image")
